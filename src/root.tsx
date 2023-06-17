96bb69954c65c9cd704bc386b05ced0161003f0e
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-06-17 15:46:05 -0400

    chore: :construction: Move router-head component to shared folder and format

--- src/root.tsx
+++ src/root.tsx
@@ -1,29 +1,22 @@
-import { component$ } from "@builder.io/qwik"
+import { component$ } from '@builder.io/qwik'
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
-} from "@builder.io/qwik-city"
-import { RouterHead } from "./components/router-head/router-head"
+} from '@builder.io/qwik-city'
+import { RouterHead } from './shared/components/router-head/router-head'
 
-import "./global.css"
+import './global.css'
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Dont remove the `<head>` and `<body>` elements.
-   */
-
   return (
     <QwikCityProvider>
       <head>
-        <meta charSet="utf-8" />
-        <link rel="manifest" href="/manifest.json" />
+        <meta charSet='utf-8' />
+        <link rel='manifest' href='/manifest.json' />
         <RouterHead />
       </head>
-      <body lang="en">
+      <body lang='en' class='bg-slate-800 text-slate-100'>
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
