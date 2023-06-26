51319a9ffb1ed78c42ea90966069de5067e4747e
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-06-26 19:00:56 -0400

    fix: :bug: Protected route with profile with not user

--- src/auth/helpers/protected-route.ts
+++ src/auth/helpers/protected-route.ts
@@ -1,25 +1,41 @@
 import type { RequestEvent } from '@builder.io/qwik-city'
 import { COOKIES_ACCESS_TITLE } from '../constants'
-import { getUserFromServer } from '../services'
+import { getProfileFromServer } from '../services'
 
 export const protectedRoute = async ({
   cookie,
   next,
   redirect,
   env,
+  params,
 }: RequestEvent<QwikCityPlatform>) => {
   const token = cookie.get(COOKIES_ACCESS_TITLE)?.value
 
-  if (!token) {
+  const privateKey = env.get('PRIVATE_SUPABASE_KEY')
+
+  if (!token || !privateKey) {
     redirect(303, '/auth/sign-in')
     return
   }
 
-  const privateKey = env.get('PRIVATE_SUPABASE_KEY')
+  console.log('Hay Tokens')
 
-  const user = await getUserFromServer({ privateKey, jwt: token })
+  const profile = await getProfileFromServer({ privateKey, token })
 
-  if (user?.data?.user?.role !== 'free') {
+  if (!profile || !profile.id || !profile.email || !profile.role) {
+    redirect(303, '/auth/sign-in')
+    return
+  }
+
+  const name = profile.email.split('@')[0]
+  console.log({ name, params: params.user })
+
+  if (params.user !== name) {
+    redirect(303, '/auth/sign-in')
+    return
+  }
+
+  if (profile.role === 'free') {
     await next()
   } else {
     redirect(303, '/auth/sign-in')
--- src/auth/services/getUser.ts
+++ src/auth/services/getUser.ts
@@ -15,3 +15,47 @@ export async function getUserFromServer({
 }) {
   return await supabaseServer(privateKey!).auth.getUser(jwt!)
 }
+
+export type ProfileFromServer = {
+  id: string
+  email: string | undefined
+  role: string | null
+}
+
+export async function getProfileFromServer({
+  privateKey,
+  token,
+}: {
+  privateKey: string
+  token: string
+}): Promise<ProfileFromServer> {
+  const profileFromServer: ProfileFromServer = {
+    id: '',
+    email: undefined,
+    role: null,
+  }
+
+  const { data, error } = await getUserFromServer({ privateKey, jwt: token })
+
+  if (error || !data?.user?.id || !data?.user?.email) return profileFromServer
+
+  const { id, email } = data.user
+
+  if (!id || !email) return profileFromServer
+
+  const { data: profile, error: profileError } = await supabaseServer(
+    privateKey
+  )
+    .from('profile')
+    .select('id, role')
+    .eq('id', id)
+    .limit(1)
+
+  if (profileError || !profile) return profileFromServer
+
+  profileFromServer.id = id
+  profileFromServer.email = email!
+  profileFromServer.role = profile[0].role
+
+  return profileFromServer
+}
