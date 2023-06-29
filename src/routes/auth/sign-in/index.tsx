bc9fd981afaa1bbfef6c4f4d448a7052b039de0b
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-06-29 19:50:44 -0400

    build: :rocket: Modify wrangler tom

--- src/routes/auth/sign-in/index.tsx
+++ src/routes/auth/sign-in/index.tsx
@@ -3,10 +3,19 @@ import { component$ } from '@builder.io/qwik'
 import { SignIn } from '~/auth/component'
 import { AUTH_SIGN } from '~/auth/constants'
 import { useSignAuth } from '~/auth/hooks'
+import { MESSAGE_TYPE, useMessageContext } from '~/message'
 
 export default component$(() => {
+  const { setMessage } = useMessageContext()
   const { handlerGithub, handlerGoogle, handlerSubmit, stateAuth } =
     useSignAuth(AUTH_SIGN.IN)
+
+  const url = import.meta.env.PUBLIC_SUPABASE_URL
+  const publicKey = import.meta.env.PUBLIC_SUPABASE_KEY
+  setMessage({
+    message: `ur: ${url}, key: ${publicKey}`,
+    type: MESSAGE_TYPE.INFO,
+  })
 
   return (
     <SignIn
--- src/routes/auth/sign-up/index.tsx
+++ src/routes/auth/sign-up/index.tsx
@@ -3,10 +3,20 @@ import { component$ } from '@builder.io/qwik'
 import { SignUp } from '~/auth/component'
 import { AUTH_SIGN } from '~/auth/constants'
 import { useSignAuth } from '~/auth/hooks'
+import { MESSAGE_TYPE, useMessageContext } from '~/message'
 
 export default component$(() => {
   const { stateAuth, handlerGithub, handlerGoogle, handlerSubmit } =
     useSignAuth(AUTH_SIGN.UP)
+
+  const { setMessage } = useMessageContext()
+
+  const url = import.meta.env.PUBLIC_SUPABASE_URL
+  const publicKey = import.meta.env.PUBLIC_SUPABASE_KEY
+  setMessage({
+    message: `ur: ${url}, key: ${publicKey}`,
+    type: MESSAGE_TYPE.INFO,
+  })
 
   return (
     <SignUp
